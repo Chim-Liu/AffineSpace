ae92ac994f5afc5f45ef241b8b7cd29cf4950d5d
ycl <l1807005866@gmail.com>
2024-05-08 14:14:13 -0400

    P+V&V+V

--- Main.lean
+++ Main.lean
@@ -5,23 +5,26 @@ import Mathlib.Algebra.AddTorsor
 import Mathlib.Data.Vector        --https://github.com/leanprover-community/mathlib4/blob/a7ed535af2a1f78accefeaeee98233dd25714110/Mathlib/Data/Vector.lean#L20-L21
 
 -- Define some ℚ n-tuples
-def t1 : Vector ℚ 3 := ⟨ [0, (1/2:ℚ), 2], rfl ⟩ 
-def t2 : Vector ℚ 3 := ⟨ [2, (-1/2:ℚ), -2], rfl ⟩ 
+def t1 : Vector ℚ 3 := ⟨ [0, (1/2:ℚ), 2], rfl ⟩
+def t2 : Vector ℚ 3 := ⟨ [2, (-1/2:ℚ), -2], rfl ⟩
 
--- see tuple toString delegate to List toString 
+-- see tuple toString delegate to List toString
 #eval s!"{t1}"
 
 
 -- Define some rational affine 3-points
-def p1 : AffPoint ℚ 3  := ⟨ t1 ⟩ 
-def p2 : AffPoint ℚ 3  := ⟨ t2 ⟩ 
+def p1 : AffPoint ℚ 3  := ⟨ t1 ⟩
+def p2 : AffPoint ℚ 3  := ⟨ t2 ⟩
 
 -- AffPt toString
 #eval s!"{p1}"
 def v := p2 -ᵥ p1
-
+def p3 := v +ᵥ p2
+def v2 :=p1 -ᵥ p2
+def v3 := v +  v2
 #eval s!"{v}"
-
+#eval s!"{p3}"
+#eval s!"{v3}"
 
 #check ToString (AffPoint ℚ 3)
 
